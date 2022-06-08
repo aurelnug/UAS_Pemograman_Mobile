85c5d4c65e865f5df0341d8247e7b4b15bf69aab
Aurelia <aureliaamonica@gmail.com>
2022-06-08 09:12:14 +0100

    Memperbaiki Error

--- app/src/main/java/com/example/uas_pemograman_mobile/ui/RumahSakit_covid19/RumahSakitDetailFragment.kt
+++ app/src/main/java/com/example/uas_pemograman_mobile/ui/RumahSakit_covid19/RumahSakitDetailFragment.kt
@@ -1,32 +1,32 @@
-package com.example.uas_pemograman_mobile.ui.RumahSakit_covid19
-
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.MenuItem
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
-import com.example.uas_pemograman_mobile.R
-import com.example.uas_pemograman_mobile.databinding.FragmentRsDetailBinding
-import com.example.uas_pemograman_mobile.ui.MyViewModel
-
-
-class RumahSakitDetailFragment: Fragment() {
-    private val viewModelrs: MyViewModel by activityViewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val binding = FragmentRsDetailBinding.inflate(inflater)
-
-        binding.lifecycleOwner = this
-        binding.viewModel = viewModelrs
-
-        return binding.root
-    }
-
-}
+//package com.example.uas_pemograman_mobile.ui.RumahSakit_covid19
+//
+//import android.os.Bundle
+//import android.view.LayoutInflater
+//import android.view.MenuItem
+//import android.view.View
+//import android.view.ViewGroup
+//import androidx.fragment.app.Fragment
+//import androidx.fragment.app.activityViewModels
+//import androidx.navigation.fragment.findNavController
+//import com.example.uas_pemograman_mobile.R
+//import com.example.uas_pemograman_mobile.databinding.FragmentRsDetailBinding
+//import com.example.uas_pemograman_mobile.ui.MyViewModel
+//
+//
+//class RumahSakitDetailFragment: Fragment() {
+//    private val viewModelrs: MyViewModel by activityViewModels()
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        val binding = FragmentRsDetailBinding.inflate(inflater)
+//
+//        binding.lifecycleOwner = this
+//        binding.viewModel = viewModelrs
+//
+//        return binding.root
+//    }
+//
+//}
